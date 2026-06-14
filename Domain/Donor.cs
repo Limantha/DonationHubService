fb64425feacb2ffae37329caa40acb40dfabaa7f
Limantha <limanthafb@gmail.com>
2026-06-14 12:47:55 -0400

    Add project files.

---
+++ Domain/Donor.cs
@@ -0,0 +1,11 @@
+namespace Domain
+{
+    public class Donor
+    {
+        public int Id { get; set; }
+
+        public string FullName { get; set; } = string.Empty;
+
+        public string EMail { get; set; } = string.Empty;
+    }
+}
